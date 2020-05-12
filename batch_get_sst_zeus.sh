5801d203ee34a6d96ae45261b074f41c3152f008
mrayson <matt.rayson@gmail.com>
2020-05-12 09:53:36 +0800

    Added some night time notebooks

--- batch_get_sst_zeus.sh
+++ batch_get_sst_zeus.sh
@@ -26,11 +26,11 @@ PYTHON=python
 #$PYTHON -u download_sst.py -s 2017,6,11,0,0 -d 45 > sst_201706.out &
 #$PYTHON -u download_sst.py -s 2017,9,4,0,0 -d 180 > sst_201709.out &
 #$PYTHON -u download_sst.py -s 2016,1,1,0,0 -d 31 > sst_201601.out &
-$PYTHON -u download_sst_2018.py -s 2018,3,1,0,0 -d 31 > sst_201803_a.out &
-$PYTHON -u download_sst_2018.py -s 2018,4,1,0,0 -d 30 > sst_201804_a.out &
-$PYTHON -u download_sst_2018.py -s 2018,5,1,0,0 -d 31 > sst_201805_a.out &
-$PYTHON -u download_sst_2018.py -s 2018,6,1,0,0 -d 30 > sst_201806_a.out &
-$PYTHON -u download_sst_2018.py -s 2018,7,1,0,0 -d 31 > sst_201807_a.out &
+#$PYTHON -u download_sst_2018.py -s 2018,3,1,0,0 -d 31 > sst_201803_a.out &
+#$PYTHON -u download_sst_2018.py -s 2018,4,1,0,0 -d 30 > sst_201804_a.out &
+#$PYTHON -u download_sst_2018.py -s 2018,5,1,0,0 -d 31 > sst_201805_a.out &
+#$PYTHON -u download_sst_2018.py -s 2018,6,1,0,0 -d 30 > sst_201806_a.out &
+#$PYTHON -u download_sst_2018.py -s 2018,7,1,0,0 -d 31 > sst_201807_a.out &
 #$PYTHON -u download_sst.py -s 2017,10,1,0,0 -d 31 > sst_201710.out &
 #$PYTHON -u download_sst.py -s 2016,8,1,0,0 -d 31 > sst_201608.out &
 #$PYTHON -u download_sst.py -s 2016,9,1,0,0 -d 30 > sst_201609.out &
@@ -38,11 +38,11 @@ $PYTHON -u download_sst_2018.py -s 2018,7,1,0,0 -d 31 > sst_201807_a.out &
 #$PYTHON -u download_sst.py -s 2016,11,1,0,0 -d 30 > sst_201611.out &
 #$PYTHON -u download_sst.py -s 2016,12,1,0,0 -d 31 > sst_201612.out &
 
-#YEAR=$1
-#$PYTHON -u download_sst.py -s $YEAR,1,1,0,0 -d 31 > sst_"$YEAR"01.out &
-#$PYTHON -u download_sst.py -s $YEAR,2,1,0,0 -d 28 > sst_"$YEAR"02.out &
-#$PYTHON -u download_sst.py -s $YEAR,3,1,0,0 -d 31 > sst_"$YEAR"03.out &
-#$PYTHON -u download_sst.py -s $YEAR,4,1,0,0 -d 30 > sst_"$YEAR"04.out &
+YEAR=$1
+$PYTHON -u SCRIPTS/download_sst.py -s $YEAR,1,1,0,0 -d 31 > sst_"$YEAR"01.out &
+$PYTHON -u SCRIPTS/download_sst.py -s $YEAR,2,1,0,0 -d 28 > sst_"$YEAR"02.out &
+$PYTHON -u SCRIPTS/download_sst.py -s $YEAR,3,1,0,0 -d 31 > sst_"$YEAR"03.out &
+$PYTHON -u SCRIPTS/download_sst.py -s $YEAR,4,1,0,0 -d 30 > sst_"$YEAR"04.out &
 #$PYTHON -u download_sst.py -s $YEAR,5,1,0,0 -d 31 > sst_"$YEAR"05.out &
 #$PYTHON -u download_sst.py -s $YEAR,6,1,0,0 -d 30 > sst_"$YEAR"06.out &
 #$PYTHON -u download_sst.py -s $YEAR,7,1,0,0 -d 31 > sst_"$YEAR"07.out &
